757540f0e335d5cbe98ed190bb45bd39feba218c
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-09-25 10:45:43 -0700

    dropdown button toggles classlist: visible

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import "./styles/main.scss";
 import {colorado, iceland, waterfall, autumn, sea, flowers, creek, mountains} from "./carousel";
 
 const body = document.body;
-const page = document.createElement("div");
-const header = document.createElement("div");
-const main = document.createElement("div");
-const footer = document.createElement("div");
 
-body.append(page);
-page.append(header);
-page.append(main);
-page.append(footer);
+// dropdown
+const divMenu = document.querySelector('.dropdown-container')
+const listMenu = document.querySelector('.dropdown-menu')
+const btnMenu = document.querySelector('.menu-title')
+
+btnMenu.addEventListener("click", (e) => {
+    if (e.target === e.currentTarget) {
+      listMenu.classList.toggle("visible");
+    }  
+  })
+
+window.addEventListener("click", (e) => {
+    if (!divMenu.contains(e.target)) {
+      listMenu.classList.remove("visible")
+    }
+  })
